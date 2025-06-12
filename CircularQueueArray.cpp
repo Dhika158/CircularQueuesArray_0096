568eb82f517b465651dcbcfaf9049964a2c2a5e6
Dhika158 <andhikanurjamil7@gmail.com>
2025-06-12 17:40:56 +0700

    1.a

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -23,6 +23,9 @@ class Queues
         cout << endl;
 
         // 1. Cek apakah antrian penuh 
-        
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; // 1.a
+        }
     }
 }
