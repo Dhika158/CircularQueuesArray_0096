cae8bcad9a43da6f5173684969ad2cdc92447907
Dhika158 <andhikanurjamil7@gmail.com>
2025-06-12 18:34:54 +0700

    FRONT posisition

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -92,6 +92,20 @@ class Queues
                 cout << queue_array[FRONT_position] << "  ";
                 FRONT_position++;
             }
+            cout << endl;
+        }
+        else
+        {
+            // Jika FRONT_position > REAR_position, interasi dari FRONT hingga akhir array
+            while (FRONT_position <= max -1 )
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position
+            }
+
+            FRONT_position = 0;
+
+            
         }
     }
 }
