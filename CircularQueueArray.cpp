4492136f1a4739863eee04d83623f31bfbf26cac
Dhika158 <andhikanurjamil7@gmail.com>
2025-06-12 17:34:16 +0700

    Enter a number

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -7,4 +7,17 @@ class Queues
     static const int max = 5;
     int FRONT, REAR;
     int queue_array[5];
+
+    public: 
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+    }
 }
