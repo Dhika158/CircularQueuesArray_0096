44a28c51bdf3e5334fa759f606018a0af9966e69
Dhika158 <andhikanurjamil7@gmail.com>
2025-06-12 18:51:44 +0700

    Check for the values entered

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -158,9 +158,16 @@ int main()
                 default:
                 {
                     cout << "Invalid option!!" << endl;
+                    break;
                 }
             }
             
         }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
     }
+
+    return 0;
 }
